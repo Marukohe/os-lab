91fe5ffedcbc67152b03fd738717943ac05b75f0
tracer-nju <tracer@nju.edu.cn>
2019-02-26 18:58:43 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 6e89cc4a3e4e4562b26cdee2a0354a3a   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  18:58:43 up  2:25,  2 users,  load average: 0.39, 0.65, 0.87

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#define DEFAULT_DIR "/proc/"
 
 void output_version(){
-	printf("pstree1.0 made by HEWEI\n");
+	printf("pstree1.0 made by HEWEI.\n");
+}
+
+void read_list(const char *dirPath){
+	DIR * dir;
+	dir = opendir(dirPath);
+	struct dirent * file;
+	while((file = readdir(dir))!=NULL){
+		print("filename: %s\n",file.d_name);
+	}
 }
 
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
   int i;
-  for (i = 0; i < argc; i++) {
-    assert(argv[i]); // always true
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
+  //for (i = 0; i < argc; i++) {
+  //  assert(argv[i]); // always true
+  //  printf("argv[%d] = %s\n", i, argv[i]);
+  //}
   if(strcmp(argv[1],"-V")==0)
 	  output_version();
+  read_list("~/NJU/");
   assert(!argv[argc]); // always true
   return 0;
 }
