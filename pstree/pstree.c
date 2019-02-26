561343edc76a295b0be9b421b45e6e15b8e6551f
tracer-nju <tracer@nju.edu.cn>
2019-02-26 17:51:25 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 6e89cc4a3e4e4562b26cdee2a0354a3a   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  17:51:25 up  1:17,  1 user,  load average: 0.63, 0.79, 0.88

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include <assert.h>
 
+void output_version(){
+	printf("pstree1.0 made by HEWEI\n");
+}
+
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
   int i;
@@ -9,8 +13,8 @@ int main(int argc, char *argv[]) {
     assert(argv[i]); // always true
     printf("argv[%d] = %s\n", i, argv[i]);
   }
-  if(strcmp(argv[1],"V")==0)
-  	printf("pstree1.0 made by HEWEI");
+  if(strcmp(argv[1],"-V")==0)
+	  output_version();
   assert(!argv[argc]); // always true
   return 0;
 }
