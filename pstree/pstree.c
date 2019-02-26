4f54300c13e4a1ae7c18c747d97c4b56857a6c5a
tracer-nju <tracer@nju.edu.cn>
2019-02-26 19:02:58 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 6e89cc4a3e4e4562b26cdee2a0354a3a   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  19:02:58 up  2:29,  2 users,  load average: 0.48, 0.62, 0.81

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -21,14 +21,14 @@ void read_list(const char *dirPath){
 
 int main(int argc, char *argv[]) {
   printf("Hello, World!\n");
-  //int i;
-  //for (i = 0; i < argc; i++) {
-  //  assert(argv[i]); // always true
-  //  printf("argv[%d] = %s\n", i, argv[i]);
-  //}
+  int i;
+  for (i = 0; i < argc; i++) {
+    assert(argv[i]); // always true
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
   if(strcmp(argv[1],"-V")==0)
 	  output_version();
-  read_list("~/NJU/");
+ // read_list("~/NJU/");
   assert(!argv[argc]); // always true
   return 0;
 }
