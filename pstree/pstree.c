bfeea62228e7b19ace3f165289cd2cdb49949a92
tracer-nju <tracer@nju.edu.cn>
2019-02-27 14:10:35 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 8763eca8a98442dfb844bdd003589da0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  14:10:35 up 20 min,  3 users,  load average: 0.58, 0.65, 0.53

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -118,8 +118,11 @@ void read_list(const char *dirPath){
 	closedir(dir);
 }
 
-void print_tree(){
-	printf("\n");
+void print_tree(bool is_n, bool is_p){
+	for(int i=0;i<MAX_LEN;i++){
+		if(P[i].pid!=0)
+			printf("pid: %d, fa: %d, name: %s\n",P[i].pid, P[i].fa,P[i].pidname);
+	}
 }
 
 int main(int argc, char *argv[]) {
