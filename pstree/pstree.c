5b92da9f5be46771a62f9324f0ed6db16aedbcbc
tracer-nju <tracer@nju.edu.cn>
2019-02-27 15:16:18 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 8763eca8a98442dfb844bdd003589da0   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 4.15.0-45-generic       Architecture: x86-64  15:16:18 up  1:26,  4 users,  load average: 0.64, 0.59, 0.53

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -86,7 +86,7 @@ void read_list(const char *dirPath){
 				int size = strlen(str);
 				if(strncmp(str,"Name",4)==0){
 					int k=0;
-					for(int i = 5;i<size;i++){
+					for(int i = 4;i<size;i++){
 						if(str[i] != 32 && str[i]!=13 && str[i]!=10)
 							P[pidnum].pidname[k++] = str[i];
 			 		}
