ecbe8229996f9a1eab1afe8dca2044b8f091e2b1
171240537-He Wei <1502449818@qq.com>
2019-03-03 15:40:42 +0800

    copy string.c an stdio.c to klib

--- abstract-machine/libs/klib/src/stdio.c
+++ abstract-machine/libs/klib/src/stdio.c
@@ -3,20 +3,298 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  return 0;
+#define is_digit(c) ((c) >= '0' && (c) <='9')
+#define ZEROPAD 1
+#define SIGN 2
+#define PLUS 4
+#define SPACE 8
+#define LEFT 16
+#define SPECIAL 32
+#define LARGE 64
+static int skip_atoi(const char **s)
+{
+	int i=0;
+	while(is_digit(**s)){
+		i = i + *((*s)++)-'0';
+	}
+	return i;
+}
+
+
+static char *digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+static char *upper_digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+static char *num_to_string(char *str, long num,int base,int size,int precision,int type){
+	char c,sign,tmp[66];
+	char *dig = digits;
+	int i;
+	if(type&LARGE) dig = upper_digits;
+	if(type&LEFT) type &= ~ZEROPAD;
+	if(base<2||base>36) return 0;
+
+	c = (type & ZEROPAD) ? '0' : ' ';
+	sign = 0;
+	if(type&SIGN){
+		if(num<0){
+			sign = '-';
+			num = -num;
+			size--;
+		}
+		else if(type & PLUS){
+			sign = '+';
+			size--;
+		}
+		else if(type&SPACE){
+			sign = ' ';
+			size--;
+		}
+	}
+
+	if(type&SPECIAL){
+		if(base==16)
+			size -=2;
+		else if(base==8)
+			size--;
+	}
+	i=0;
+	if(num==0)
+		tmp[i++]='0';
+	else{
+		while(num!=0){
+			tmp[i++] = dig[((unsigned long) num) % (unsigned) base];
+			num = ((unsigned long) num) / (unsigned) base;
+		}
+	}
+
+	if(i>precision) precision=i;
+	size -= precision;
+	if(!(type&(ZEROPAD|LEFT)))
+	{
+		while(size-->0) *str++=' ';
+	}
+	if(sign) *str++ = sign;
+
+	if(type &SPECIAL){
+		if(base==8)
+			*str++='0';
+		else if(base==16)
+		{
+			*str++='0';
+			*str++=digits[33];
+		}
+	}
+
+	if(!(type&LEFT)){
+		while(size-->0) *str++=c;
+	}
+
+	while(i<precision--) *str++='0';
+	while(i-->0) *str++=tmp[i];
+	while(size-->0) *str++=' ';
+
+	return str;
+
+		
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+	char *str;
+	int field_width;
+
+	for(str=out;*fmt;fmt++){
+		unsigned long num;
+		int base = 10;
+		int flags = 0;
+		int qualifier = -1;
+		int precision = -1;
+	    int bFmt = 1;
+		if(*fmt != '%'){
+			*str++=*fmt;
+			continue;
+		}
+		bFmt = 1;
+		while(bFmt){
+			fmt++;
+			switch(*fmt){
+				case '-': flags |= LEFT; break;
+				case '+': flags |= PLUS; break;
+				case ' ': flags |= SPACE; break;
+				case '#': flags |= SPECIAL; break;
+				case '0': flags |= ZEROPAD; break;
+				default: bFmt = 0;
+			}
+		} 
+		field_width = -1;
+		if(is_digit(*fmt))
+			field_width = skip_atoi(&fmt);
+		else if('*'==*fmt){
+			fmt++;
+			field_width = va_arg(ap,int);
+			if(field_width<0){
+				field_width = -field_width;
+				flags |= LEFT;
+		 	}
+		}  
+
+		precision=-1;
+		if('.'==*fmt){
+			fmt++;
+			if(is_digit(*fmt)){
+				precision = skip_atoi(&fmt);
+			}
+			else if('*'==*fmt){
+				fmt++;
+				precision = va_arg(ap,int);
+			}
+			if(precision<0) precision=0;
+		}
+
+		qualifier = -1;
+		if('h' == *fmt || 'l'==*fmt || 'L' ==*fmt){
+			qualifier = *fmt;
+			fmt++;
+		}
+
+		base = 10;
+		switch(*fmt){
+			case 'c':
+			{
+				if(!(flags & LEFT)) while (--field_width>0) *str++=' ';
+				*str++ = (unsigned char) va_arg(ap,int);
+				while(--field_width>0) *str++=' ';
+				continue;
+			}
+			case 's':
+			{
+				int len;
+				char *s = va_arg(ap,char *);
+				if(!s) s = "<NULL>";
+				//len = strnlen(s,precision);
+				len = strlen(s);
+				if(!(flags & LEFT)) while(len<field_width--) *str++ = ' ';
+				for(int i=0;i<len;i++) *str++ = *s++;
+				while(len<field_width--) *str++=' ';
+				continue;
+			}
+			case 'p':
+			{
+				if(field_width==-1)
+				{
+					field_width=2*sizeof(void*);
+					flags|=ZEROPAD;
+				}
+				str=num_to_string(str,(unsigned long)va_arg(ap,void *),16,field_width,precision,flags);
+				continue;
+			}
+			case 'n':
+			{
+				if(qualifier=='1')
+				{
+					long *ip=va_arg(ap,long *);
+					*ip=(str-out);
+				}
+				else
+				{
+					int *ip=va_arg(ap,int *);
+					*ip=(str-out);
+				}
+				continue;
+			}
+			case 'o':
+			{
+				base = 8;
+				break;
+			}
+			case 'X':
+			{
+				flags |= LARGE;
+			}
+			case 'x':
+			{
+				base = 16;
+				break;
+			}
+			case 'd':
+			case 'i':
+			{
+				flags |= SIGN;
+			}
+			case 'u':
+			{
+				break;
+			}
+			default:
+			{
+				if(*fmt != '%') *str++ = '%';
+				if(*fmt)
+					*str++=*fmt;
+				else
+					--fmt;
+				continue;
+			}
+		}
+
+		if(qualifier == 'l')
+			num = va_arg(ap,unsigned long);
+		else if(qualifier == 'h'){
+			if(flags &SIGN)
+				num = va_arg(ap,int);//short int
+			else
+				num = va_arg(ap,unsigned int);//unsignedshort int
+		}
+		else if(flags &SIGN)
+			num=va_arg(ap,int);
+		else
+			num = va_arg(ap,unsigned long);
+
+		str = num_to_string(str,num,base,field_width,precision,flags);
+	}
+	*str = '\0';
+	return str - out;
 }
 
+
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+	va_list args;
+	int res;
+	va_start(args,fmt);
+	res = vsprintf(out,fmt,args);
+	va_end(args);
+	return res;
+}
+
+int printf(const char *fmt, ...) {
+	va_list args;
+	int res;
+	va_start(args,fmt);
+	char s[256];
+	res=vsprintf(s,fmt,args);
+	va_end(args);
+	//size_t cnt=strlen(s);
+	for(int i=0;i<res;i++){
+		_putc(s[i]);
+	}
+	return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  return 0;
+  va_list args;
+  //int res;
+  char buf[256];
+  va_start(args,fmt);
+  vsprintf(buf,fmt,args);
+  va_end(args);
+  //printf("%s\n",buf);
+  //printf("%s\n\n\n\n\n\n\n\n",buf);
+  //memcpy(out,buf,n);
+  //char dst[65536];
+  out=strncpy(out,(const char *)buf,n);
+  //strcat(out,(const char *)dst);
+  //char *s1;
+  //s1 = out;
+  //s1 +=n;
+  //s1 = '\0';
+  return n;
 }
 
 #endif
--- abstract-machine/libs/klib/src/string.c
+++ abstract-machine/libs/klib/src/string.c
@@ -3,39 +3,107 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+	size_t i = 0;
+	while(s[i]!='\0')
+		i++;
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+	size_t i;
+	//size_t sizesrc=strlen(src);
+	for( i=0;src[i]!='\0';i++)
+	   dst[i]=src[i];
+	dst[i]='\0';
+	return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+	size_t i;
+	for(i = 0; i < n && src[i] != '\0'; i++)
+		dst[i]=src[i];
+	for( ; i<n; i++)
+		dst[i]='\0';
+	return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+	size_t dest_len = strlen(dst);
+	size_t i;
+
+	for(i=0;src[i]!='\0';i++)
+		dst[dest_len+i]=src[i];
+	dst[dest_len+i]='\0';
+	return dst;
 }
 
-int strcmp(const char* s1, const char* s2) {
-  return 0;
+int strcmp(const char* s1,const char*s2){
+	for(;*s1==*s2;++s1,++s2)
+		if(*s1=='\0')
+			return 0;
+	return ((*(unsigned char*)s1<*(unsigned char*)s2)?-1:1);
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+	size_t i;
+	int value=0;
+	for(i=0;i<n;i++)
+	{
+		if(s1[i]-'\0'==s2[i]-'\0')
+			continue;
+		else
+		{
+			if(s1[i]-'\0'<s2[i]-'\0')
+			{
+				value=-1;
+				return value;
+			}
+			else return 1;
+		} 
+	}
+	return value;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+	const unsigned char ch = c;
+	unsigned char *ret;
+	for(ret=v;0<n;++ret,--n)
+		*ret=ch;
+	return v;
 }
 
-void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
-}
+void* memcpy(void* out, const void* in, size_t n){
+	char *s1;
+	const char *s2;
+	for(s1=out,s2=in;0<n;++s1,++s2,--n)
+		*s1=*s2;
+	//*s1 = '\0';
+	return out;	
+} 
 
 int memcmp(const void* s1, const void* s2, size_t n){
-  return 0;
+	if(s1==NULL||s2==NULL||n<0)
+	   return 0;
+	const unsigned char* dst = (const unsigned char*)s1;
+	const unsigned char* src = (const unsigned char*)s2;
+	while(n--){
+		while(*dst==*src)
+		{
+			if(*dst=='\0')
+				return 0;
+			dst++;
+			src++;
+		}
+		//if(n<=0)
+		//	return 0;
+	}
+	if(n==-1)
+		return 0;
+	if(*dst>*src)
+		return 1;
+	else if(*dst<*src)
+		return -1;
+	return 0;
 }
 
 #endif
