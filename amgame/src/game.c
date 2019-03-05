b98343881a6ba36906695aba69c32794155a5c76
tracer-nju <tracer@nju.edu.cn>
2019-03-05 19:02:40 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 1eb4df98b6ec416593276f756cc918f3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  19:02:40 up 44 min,  3 users,  load average: 0.38, 0.31, 0.26

--- amgame/src/game.c
+++ amgame/src/game.c
@@ -74,7 +74,7 @@ void init_screen() {
   _io_read(_DEV_VIDEO, _DEVREG_VIDEO_INFO, &info, sizeof(info));
   w = info.width;
   h = info.height;
-  w = 416;
+  w = 415;
   Log("w: %d h: %d side: %d",w,h,SIDE);
 }
 
