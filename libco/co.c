7ce0d3d41a5302f705309e1e85e185e21f529a28
tracer-nju <tracer@nju.edu.cn>
2019-03-16 21:29:57 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 33d4095dd0974b67b544d2a4fb901d2f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  21:29:57 up 11:46,  6 users,  load average: 0.27, 0.20, 0.15

--- libco/co.c
+++ libco/co.c
@@ -105,7 +105,7 @@ void co_yield() {
         while((id!=max_co||main_flag==1)&&(coroutines[id].state == SUSPEND||coroutines[id].state == FREE)){
             id = rand()%(max_co+1);
         }
-        printf("\nmainflag: %d id:  %d state: %d\n",main_flag,id,coroutines[id].state);
+        /*printf("\nmainflag: %d id:  %d state: %d\n",main_flag,id,coroutines[id].state);*/
         if(id==max_co){
             struct co *t = &coroutines[running_co];
             t->state = READY;
