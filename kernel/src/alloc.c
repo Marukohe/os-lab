6cea40082041a2b3edcef5ecabb4aa824e7301b7
tracer-nju <tracer@nju.edu.cn>
2019-03-27 10:56:51 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 0448b9d469184ab89172302d7d871b79   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  10:56:51 up  1:29,  8 users,  load average: 0.89, 0.56, 0.57

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -6,6 +6,7 @@ static uintptr_t pm_start, pm_end;
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
+  Log("pm_start %u, pm_end %u",pm_start,pm_end);
 }
 
 static void *kalloc(size_t size) {
