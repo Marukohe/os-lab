975c9cb46b1e4e01267e69df2f45303f8336aaac
tracer-nju <tracer@nju.edu.cn>
2019-03-31 15:53:12 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 32c98045c3b2401484e95f0048c5916e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  15:53:12 up  1:03,  7 users,  load average: 0.97, 0.93, 0.71

--- kernel/src/spin_lock.c
+++ kernel/src/spin_lock.c
@@ -2,11 +2,11 @@
 #include <klib.h>
 
 void spin_lock(spinlock_t lk){
-    /*cli();*/
+    cli();
     while(_atomic_xchg(&lk->locked, 1));
 }
 
-void spin_unlock(spinlock &lk){
+void spin_unlock(spinlock lk){
     _atomic_xchg(&lk->locked, 0);
     sti();
 }
