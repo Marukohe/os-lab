69de069e5deb0bea44e8ad0d78bb78f4ce8f7060
tracer-nju <tracer@nju.edu.cn>
2019-04-01 11:41:09 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 330224fa5e834524a54074da792f1bf3   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  11:41:09 up  2:45,  5 users,  load average: 1.03, 0.79, 0.59

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -1,9 +1,14 @@
 #include <common.h>
 #include <klib.h>
+#define SMALLSIZE 256
+#define CPUNUM 4
+#define BLOCK 4*1024
+#define STSIZE 40
 
 typedef struct Kmem{
     uintptr_t start;
     uintptr_t size;
+    uintptr_t maxsize;
     struct Kmem *next;
     struct Kmem *prev;
 }kmem;
@@ -11,13 +16,25 @@ typedef struct Kmem{
 static uintptr_t start;
 static uintptr_t pm_start, pm_end;
 
+kmem smem[CPUNUM];
+kmem lmem;
+
 static void pmm_init() {
   pm_start = (uintptr_t)_heap.start;
   pm_end   = (uintptr_t)_heap.end;
   Logp("pmm_init successfully, from pm_start 0x%x to pm_end 0x%x",pm_start,pm_end);
+  Logb("struct size %ld",sizeof(lmem));
 
   start = pm_start;
   lk->locked = 0;
+}
+
+static void *my_bigalloc(size_t size){
+    return NULL;
+}
+
+static void *mysmallalloc(size_t size){
+    return NULL;
 }
 
 static void *kalloc(size_t size) {
@@ -28,7 +45,15 @@ static void *kalloc(size_t size) {
     spin_unlock(lk);
     return ret;
 #else
-    return NULL;
+    spin_lock(lk);
+    void *ret;
+    if(szie > SMALLSIZE){
+        ret = my_bigalloc(size);
+    }else{
+        ret = my_smallalloc(size);
+    }
+    spin_unlock(lk);
+    return ret;
 #endif
   /*return NULL;*/
 }
