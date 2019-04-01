632a22f753e9246d10cc249a51a327e17fe9a9f6
tracer-nju <tracer@nju.edu.cn>
2019-04-01 22:31:27 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 3b6b8d814fa74c3287eccef85e4a00b6   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  22:31:27 up  4:31,  6 users,  load average: 1.41, 1.11, 1.20

--- kernel/src/alloc.c
+++ kernel/src/alloc.c
@@ -32,7 +32,7 @@ static void pmm_init() {
   assert(lk->locked==0);
   for(int i=0;i<CPUNUM;i++)
       spincnt[i] = 0;
-  assert(0);
+  /*assert(0);*/
   for(int i=0;i<CPUNUM;i++){
         //smem[i]->maxsize = 0;
         smem[i]->start = 0;
@@ -41,6 +41,7 @@ static void pmm_init() {
         smem[i]->next = NULL;
         smem[i]->prev = NULL;
   }
+  assert(0);
   assert(lk->locked==0);
   //lmem->maxsize = pm_end-pm_start;
   lmem->start = pm_start;
