6971922957151e6d3513c3d58c4d39e40ebfbfb6
171240537-He Wei <1502449818@qq.com>
2019-04-02 18:01:44 +0800

    add log colors

--- abstract-machine/libs/klib/include/klib.h
+++ abstract-machine/libs/klib/include/klib.h
@@ -78,6 +78,14 @@ int vsprintf(char *str, const char *format, va_list ap);
     printf("\33[1;32m[%s,%d,%s] " format "\33[0m\n", \
         __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
+#define Logq(format, ...) \
+    printf("\33[1;36m[%s,%d,%s] " format "\33[0m\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
+#define Logw(format, ...) \
+    printf("\33[1;37m[%s,%d,%s] " format "\33[0m\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
 // assert.h
 #ifdef NDEBUG
   #define assert(ignore) ((void)0)
