7f288e5b2b22d693c5931090e1d8d99adf756afb
tracer-nju <tracer@nju.edu.cn>
2019-04-09 22:23:36 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 3f64fb3448724c8baea2b44cb0e8728b   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  22:23:36 up 54 min,  2 users,  load average: 0.52, 0.35, 0.30

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -1,8 +1,21 @@
 #include "sperf.h"
 
 int main(int argc, char *argv[]) {
-    printf("%d\n",argc);
-    printf("%s\n",argv[0]);
-    Logy("hello");
+
+    /*printf("%d\n",argc);*/
+    /*printf("%s\n",argv[0]);*/
+    /*Logy("hello");*/
+    int childpid;
+    int i;
+    childpid = fork();
+    if(childpid == 0){
+        char * execv_str[] = {"echo", "executed by execv", NULL};
+        if(execv("/usr/bin/echo", execv_str) < 0){
+            exit(0);
+        }
+    }else{
+        wait(&chilpid);
+        printf("hello\n");
+    }
   return 0;
 }
