d27026e3ee3277311661839b157aff78e2c65faa
tracer-nju <tracer@nju.edu.cn>
2019-04-10 19:28:47 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: ab96f93ff744414d9dbe9526b9c62a6e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  19:28:47 up  1:06,  5 users,  load average: 0.19, 0.31, 0.33

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -31,7 +31,8 @@ int main(int argc, char *argv[]) {
         fp = fdopen(STDIN_FILENO, "r");
         while(fgets(r_buf,MAXBUF,fp)!=NULL){
             char buf[30];
-            sscanf(r_buf, "%[<[0-9]*\\.?[0-9]+>]", buf);
+            sscanf(r_buf, "%*[^<]/%[^>]", buf);
+            /*sscanf(r_buf, "%<[0-9]*\\.?[0-9]+>", buf);*/
             printf("%s\n", buf);
             double tmp  = 0.0;
             sscanf(buf, "%lf", &tmp);
