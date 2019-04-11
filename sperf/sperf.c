6b80e0c589848ec85ff47f17e0e4cb33f4537539
tracer-nju <tracer@nju.edu.cn>
2019-04-11 14:23:12 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 292920a54284452eae2860cc7d9fea5f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  14:23:12 up 34 min,  2 users,  load average: 0.12, 0.20, 0.18

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]) {
             printf("%lf\n", tmp);
         }
 
-        printf("\n");
+        printf("tot: %d\n", tot);
         qsort(_pitems, tot, sizeof(_pitems[0]),cmp);
         for(int i = 0; i < tot; i++){
             printf("%s %lf\n",_pitems[i].name, _pitems[i].t);
