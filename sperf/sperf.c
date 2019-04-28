3f127bca3fa60806ec8cb3cec967de8a2db1d12f
tracer-nju <tracer@nju.edu.cn>
2019-04-28 22:38:03 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: d55af517464845b785e0348fd1300b74   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  22:38:03 up  3:24,  2 users,  load average: 0.92, 1.03, 0.99

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -136,7 +136,7 @@ int main(int argc, char *argv[]) {
         }
 
         if(printfflag == 1){
-            printf("\033C");
+            printf("\033c");
         }
 
         for(int i = 0; i < tot; i++){
