fa91554b3073f516f277627428b671f1de8431b2
tracer-nju <tracer@nju.edu.cn>
2019-05-02 22:07:25 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 86ccd282443a41fe963d02fc3e1b8a44   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  22:07:25 up  1:39,  2 users,  load average: 1.30, 0.91, 0.89

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -9,9 +9,12 @@ char s[maxlen];
 int main(int argc, char *argv[]) {
     printf(">> ");
     printf("Welcome to the C-shell, Please enter quit() to quit!\n");
+    printf(">> ");
     while(~scanf("%s", s)){
         if(strcmp(s, "quit()") == 0)
             break;
+        printf(">> hello, world\n");
+        printf(">> ");
     }
   return 0;
 }
