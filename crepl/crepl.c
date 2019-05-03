170a71eccfe68a3ceeb0cf3c1b643e608b5a41b9
tracer-nju <tracer@nju.edu.cn>
2019-05-03 11:11:00 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 94a6e4570aa94c42a416dec740a0a041   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  11:11:00 up 13 min,  3 users,  load average: 0.40, 0.40, 0.25

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -5,12 +5,12 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #define maxlen 1000
-#define MODE 775
+/*#define MODE 775*/
 char s[maxlen];
 
 int main(int argc, char *argv[]) {
     char *path = "./tmpc";
-    mkdir(path, MODE);
+    mkdir(path, S_IRWXU | S_IRWXG | S_IROTH);
     printf(">> ");
     printf("Welcome to the C-shell, Please enter quit() to quit!\n");
     printf(">> ");
