d827ebbc4682fef205dcc70d83d5368207fe1dc0
tracer-nju <tracer@nju.edu.cn>
2019-05-03 11:27:48 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 94a6e4570aa94c42a416dec740a0a041   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  11:27:48 up 30 min,  4 users,  load average: 0.22, 0.27, 0.31

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -9,8 +9,17 @@
 /*#define MODE 775*/
 char s[maxlen];
 
+char * num_to_string(int cnt){
+    char *fpath = "./tmpc/";
+    sprintf(fpath, "%d", cnt);
+    strcpy(fpath, ".c");
+    printf("%s\n", fpath);
+    return fpath;
+}
+
 int main(int argc, char *argv[]) {
     char *path = "./tmpc";
+    int cnt = 0;
     /*assert(mkdir(path, 0777) == 0);*/
     int a = mkdir(path, 0777);
     printf("%d\n", a);
@@ -23,6 +32,8 @@ int main(int argc, char *argv[]) {
         if(strncmp(s, "int", 3) == 0){
             printf(">> add function successfully!\n");
             printf(">> ");
+            cnt++;
+            char *fpath = num_to_string(cnt);
         }else{
             printf(">> hello, world\n");
             printf(">> ");
