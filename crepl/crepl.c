d51b3e96cf63434ecd366ce7f3ad1b45ec4bb142
tracer-nju <tracer@nju.edu.cn>
2019-05-03 13:03:21 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 94a6e4570aa94c42a416dec740a0a041   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  13:03:21 up  2:05,  5 users,  load average: 1.13, 1.32, 1.03

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -8,23 +8,14 @@
 #define maxlen 1000
 /*#define MODE 775*/
 char s[maxlen];
-/*
-char * num_to_string(int cnt){
-    char *fpath = "./tmpc/";
-    char num[1000];
-    sprintf(num, "%d", cnt);
-    strcpy(fpath, num);
-    strcpy(fpath, ".c");
-    printf("%s\n", fpath);
-    return fpath;
-}
-*/
+
 int main(int argc, char *argv[]) {
     char *path = "./tmpc";
     int cnt = 0;
     /*assert(mkdir(path, 0777) == 0);*/
     int a = mkdir(path, 0777);
-    printf("%d\n", a);
+    /*printf("%d\n", a);*/
+    assert(a == 0);
     printf(">> ");
     printf("Welcome to the C-shell, Please enter quit() to quit!\n");
     printf(">> ");
