3e3ef6eda18fe52882114cfbfd755ddcaf7709a2
tracer-nju <tracer@nju.edu.cn>
2019-05-03 15:56:38 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 94a6e4570aa94c42a416dec740a0a041   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  15:56:38 up  4:59,  1 user,  load average: 0.99, 1.02, 0.81

--- crepl/crepl.c
+++ crepl/crepl.c
@@ -25,7 +25,8 @@ int main(int argc, char *argv[]) {
     printf(">> ");
     while(fgets(s, maxlen,stdin)){
         if(strncmp(s, "quit()", 6) == 0){
-            rmdir(path);
+            /*rmdir(path);*/
+            system("rm -rf tmpc/");
             break;
         }
         if(strncmp(s, "int", 3) == 0){
