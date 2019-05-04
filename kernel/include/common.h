bf67ae60af269ceec4f419aba8c8153af84ca762
tracer-nju <tracer@nju.edu.cn>
2019-05-04 18:00:50 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 54c911d7b6f440bfaf06d8e846b7e075   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  18:00:50 up  2:01,  4 users,  load average: 1.00, 1.13, 1.20

--- kernel/include/common.h
+++ kernel/include/common.h
@@ -6,15 +6,12 @@
 #include <x86.h>
 //#include <spinlock.h>
 
-<<<<<<< HEAD
 enum BLOCKSTATE{FREE,USING,READY};
 
 //#define DEBUG
 // #define CORRECTNESS_FIRST
-=======
 struct task {};
-struct spinlock {};
+//struct spinlock {};
 struct semaphore {};
 
->>>>>>> dad0034cd442046d9cc407654dce68cdc0fd783e
 #endif
--- kernel/include/spinlock.h
+++ kernel/include/spinlock.h
@@ -1,9 +1,9 @@
-/*
+
 typedef struct spinlock{
     volatile intptr_t locked;
     char name[10];
 }spinlock;
-*/
+
 
 void spin_lock(spinlock *lk);
 void spin_unlock(spinlock *lk);
