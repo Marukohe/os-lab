ae04b885d69ef91c561b313d1bbf83b9453c646f
tracer-nju <tracer@nju.edu.cn>
2019-05-06 11:05:01 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: dec4d838621d467fb268d1772b72c171   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  11:05:01 up  2:56,  7 users,  load average: 0.75, 0.79, 0.81

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -97,6 +97,7 @@ static void os_on_irq(int seq, int event, handler_t handler) {
         cnthandler++;
     }
     for(int k = 0; k < cnthandler - 1; k++){
+        Logy("MIN: %d MAX: %d", INT_MIN, INT_MAX);
         Logy("%d %d %d", k, schandlers[k]->seq, schandlers[k + 1]->seq);
         assert(schandlers[k]->seq <= schandlers[k + 1]->seq);
     }
