e2a3d40b6c5964ab2cd1ac7417333579b432cc72
tracer-nju <tracer@nju.edu.cn>
2019-05-07 21:52:12 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 403aca4c45904046ab8f235cd1b34ca9   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  21:52:12 up  5:29,  8 users,  load average: 0.57, 0.46, 0.49

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -32,7 +32,7 @@ int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *a
     _Area sstack;
     sstack.start = (void *)task->stack;
     sstack.end = (void *)(task->stack) + ST_SIZE;
-    task->context = _kcontext(sstack, entry, arg);
+    task->context = &_kcontext(sstack, entry, arg);
 
     tottask++;
     kmt->spin_lock(&pk);
