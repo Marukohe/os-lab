af51804fa1cc62f9533381b504c56fef4380c582
tracer-nju <tracer@nju.edu.cn>
2019-05-07 17:27:31 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 403aca4c45904046ab8f235cd1b34ca9   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  17:27:31 up  1:04,  5 users,  load average: 0.15, 0.32, 0.48

--- kernel/src/handler.c
+++ kernel/src/handler.c
@@ -2,17 +2,24 @@
 #include <klib.h>
 
 #define TKNUM 20
+extern spinlock_t pk;
 extern int tottask;
 extern task_t task[TKNUM];
 extern task_t *current_task[4];
 #define current (current_task[_cpu()])
 
 _Context *kmt_context_save(_Event ev, _Context *context){
+    kmt->spin_lock(&pk);
+    assert(0);
+    kmt->spin_unlock(&pk);
     if(current) current->context = *context;
     return &current->context;
 }
 
 _Context *kmt_context_switch(_Event ev, _Context *context){
+    kmt->spin_lock(&pk);
+    assert(0);
+    kmt->spin_unlock(&pk);
     assert(!current || current->state == RUNNING);
     int tmp = current->id;
     do{
