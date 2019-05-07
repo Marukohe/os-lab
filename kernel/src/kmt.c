3609fce33eb90d08756581b066052a634bdbf8dc
tracer-nju <tracer@nju.edu.cn>
2019-05-07 10:46:08 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: d7c5771dfc8f42efa11c134325539ed5   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  10:46:08 up 45 min,  6 users,  load average: 0.42, 0.36, 0.40

--- kernel/src/kmt.c
+++ kernel/src/kmt.c
@@ -1,13 +1,14 @@
 #include <common.h>
 #include <klib.h>
 
-
 void kmt_init(){
     os->on_irq(INT_MIN, _EVENT_NULL, kmt_context_save); // 总是最先调用
     os->on_irq(INT_MAX, _EVENT_NULL, kmt_context_switch); // 总是最后调用
 }
 
 int kmt_create(task_t *task, const char *name, void (*entry)(void *arg), void *arg){
+    task->name - name;
+    task->stack = pmm->alloc(4096);
     return 1;
 }
 
