32b62e70979123eb54681cfc6783bd6a6ea20f17
tracer-nju <tracer@nju.edu.cn>
2019-05-08 15:36:20 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 768429ad00514302b4e67fd3c763995e   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  15:36:20 up  1:09,  5 users,  load average: 1.09, 0.93, 0.85

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -60,7 +60,7 @@ static void os_init() {
   pmm->init();
   kmt->init();
   /*_vme_init(pmm->alloc, pmm->free);*/
-  dev->init();
+  /*dev->init();*/
   /*
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty2");
