3a708eb7cbf38a2ff1b1195a749a7ab0683b31ec
tracer-nju <tracer@nju.edu.cn>
2019-05-09 07:52:21 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 5f3c568263f245db8c16737c98048f9c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  07:52:21 up 28 min,  5 users,  load average: 0.21, 0.77, 0.98

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -54,9 +54,10 @@ void echo_task(void *name) {
   while (1) {
     char line[128], text[128];
     sprintf(text, "(%s) $ ", name); tty_write(tty, 0, text, strlen(text));
-    int nread = tty->ops->read(tty, 0, line, strlen(line));
+    int nread = tty->ops->read(tty, 0, line, sizeof(line));
     line[nread - 1] = '\0';
-    sprintf(text, "Echo: %s.\n", line); tty_write(tty, 0, text, strlen(text));
+    sprintf(text, "Echo: %s.\n", line);
+    tty_write(tty, 0, text, strlen(text));
   }
 }
 
