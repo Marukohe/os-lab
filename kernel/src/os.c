b7a52b5cefa1c19372bdb0b7fd6f619a5c077cd0
tracer-nju <tracer@nju.edu.cn>
2019-05-09 19:19:41 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 664ed9d3660c45a39123d757179ac645   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  19:19:41 up  2:04,  4 users,  load average: 2.82, 4.79, 3.49

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -3,9 +3,9 @@
 #include <devices.h>
 
 /*#define OSDEBUG*/
-#define CONSUMER
+/*#define CONSUMER*/
 #define IDLE
-/*#define TTY*/
+#define TTY
 
 #define TKNUM 25
 extern task_t *cputask[TKNUM];
@@ -72,7 +72,7 @@ static void os_init() {
   pmm->init();
   kmt->init();
   /*_vme_init(pmm->alloc, pmm->free);*/
-  /*dev->init();*/
+  dev->init();
 
 #ifdef TTY
   kmt->create(pmm->alloc(sizeof(task_t)), "print", echo_task, "tty1");
