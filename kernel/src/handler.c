2252b9170c1c0171d0af4288cdd9a1b2ff0d971d
tracer-nju <tracer@nju.edu.cn>
2019-05-12 15:47:59 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 61cb0899b8dd408bb01b5e2a885481e2   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  15:47:59 up  6:25,  5 users,  load average: 0.74, 0.67, 0.63

--- kernel/src/handler.c
+++ kernel/src/handler.c
@@ -10,10 +10,6 @@ extern task_t *current_task[4];
 #define current (current_task[_cpu()])
 
 _Context *kmt_context_save(_Event ev, _Context *context){
-    /*kmt->spin_lock(&pk);*/
-    /*assert(0);*/
-    /*Logp("kmtsave");*/
-    /*kmt->spin_unlock(&pk);*/
     if(current) current->context = *context;
     if(!current) return context;
     return &current->context;
@@ -25,24 +21,13 @@ int flag_switch(){
         if(i!=_cpu() && current_task[i]->id == current->id)
             res = 1;
     }
-    /*res = 1;*/
     return res;
 }
 
 _Context *kmt_context_switch(_Event ev, _Context *context){
-    /*kmt->spin_lock(&pk);*/
-    /*assert(0);*/
-    /*Logg("current task: name-> %s id->%d state->%d CPU[%d]", current->name, current->id, current->state, _cpu());*/
-    /*assert(!current || current->state != FREET);*/
-    /*assert(!current);*/
-    /*kmt->spin_unlock(&pk);*/
-    /*assert(!current || current->state != FREET);*/
     if(current && current->state != WAITING){
         current->state = FREET;
     }
-    /*int tmp = current->id;*/
-    /*if(cputask[tmp]->state != WAITING)*/
-        /*cputask[tmp]->state = FREET;*/
     do{
         if(!current || current->id == tottask - 1){
             current = cputask[0];
@@ -51,7 +36,6 @@ _Context *kmt_context_switch(_Event ev, _Context *context){
         }
 #ifdef HANDDEBUG
         kmt->spin_lock(&pk);
-        /*assert(0);*/
         printf("context switch current task: name-> %s id->%d state->%d CPU[%d]\n", current->name, current->id, current->state, _cpu());
         Logq("current task: name-> %s id->%d state->%d CPU[%d]", current->name, current->id, current->state, _cpu());
         kmt->spin_unlock(&pk);
@@ -73,7 +57,6 @@ _Context *kmt_context_switch(_Event ev, _Context *context){
     current->state = RUNNING;
 
     /*kmt->spin_lock(&pk);*/
-    /*assert(0);*/
     /*Logw("current task: name-> %s id->%d state->%d cpu: %d", current->name, current->id, current->state, _cpu());*/
     /*Logb("task0 %d, task1 %d, task2 %d, task3 %d", cputask[0]->state, cputask[1]->state, cputask[2]->state, cputask[3]->state);*/
     assert(current->fence1 == 0xcc && current->fence2 == 0xcc);
