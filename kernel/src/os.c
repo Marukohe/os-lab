f65b36f91be3af3493dfbe54bbbcd035fa668a6e
tracer-nju <tracer@nju.edu.cn>
2019-05-12 11:37:22 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 61cb0899b8dd408bb01b5e2a885481e2   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  11:37:22 up  2:14,  5 users,  load average: 0.62, 1.04, 1.14

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -4,7 +4,7 @@
 
 #define OSDEBUG
 /*#define CONSUMER*/
-/*#define IDLE*/
+#define IDLE
 #define TTY
 
 #define TKNUM 25
@@ -64,7 +64,7 @@ void echo_task(void *name) {
     tty_write(tty, 0, text, strlen(text));
     int nread = tty->ops->read(tty, 0, line, sizeof(line));
     line[nread - 1] = '\0';
-    /*printf("%s\n",line);*/
+    printf("%s\n",line);
     sprintf(text, "Echo: %s.\n", line);
     tty_write(tty, 0, text, strlen(text));
   }
