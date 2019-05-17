ecd32983b640fdfb288997760741cb30704ea888
tracer-nju <tracer@nju.edu.cn>
2019-05-17 16:46:22 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 45eeb683e87f4e4895a4816c58e8943c   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  16:46:22 up  7:41,  3 users,  load average: 0.68, 0.98, 1.30

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -1,3 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define handle_error(msg) \
+    do { perror(msg); exit(EXIT_FAILURE); } while(0)
+
+unsigned long get_file_size(const char *path){
+    unsigned long filesize = -1;
+    struct stat statbuff;
+    if(stat(path, &statbuff) < 0)
+        return filesize;
+    else
+        filesize = statbuff.st_size;
+    return filesize;
+}
+
 int main(int argc, char *argv[]) {
+    void *startaddr;
+    int fd = open(argv[1], O_RDONLY);
+    if(fd == -1)
+        handle_error("open");
+    char *path = "./";
+    strcat(path, argv[1]);
+    size_t fsize = get_file_size(path);
+    printf("%ld\n", fsize);
   return 0;
 }
