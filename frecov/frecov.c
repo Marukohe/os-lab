2371e5b36f24b7f0461b0cc0ad391d2104c0bed7
tracer-nju <tracer@nju.edu.cn>
2019-05-19 15:57:27 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 82c79d54683d4b4e823776f44111b97f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  15:57:27 up  6:36,  6 users,  load average: 1.59, 1.41, 1.29

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -122,15 +122,15 @@ int main(int argc, char *argv[]) {
                     break;
                 }
 
-                for(int k = 0; k < 10; k++){
+                for(int k = 0; k < 5; k++){
                     if(tmpdic->unicode1[k] != 0xFFFF)
                         filename[filenameoffset++] = tmpdic->unicode1[k];
                 }
-                for(int k = 0; k < 12; k++){
+                for(int k = 0; k < 6; k++){
                     if(tmpdic->unicode2[k] != 0xFFFF)
                         filename[filenameoffset++] = tmpdic->unicode2[k];
                 }
-                for(int k = 0; k <4; k++){
+                for(int k = 0; k < 2; k++){
                     if(tmpdic->unicode3[k] != 0xFFFF)
                         filename[filenameoffset++] = tmpdic->unicode3[k];
                 }
