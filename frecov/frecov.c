4996cc20eeaece2eb4d57ff70d32e1c6feef5dd4
tracer-nju <tracer@nju.edu.cn>
2019-05-19 23:37:27 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: d510d24079df43f18075d5f3fd969e13   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  23:37:27 up 14 min,  1 user,  load average: 0.86, 0.59, 0.41

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -222,7 +222,7 @@ int main(int argc, char *argv[]) {
                     close(pipefds1[0]);
                     fwrite(tmpfile, sizeof(uint8_t), bmp->bfSize, stdin);
                     // write(pipefds1[1], tmpfile, bmp->bfSize);
-                    // wait(&childpid);
+                    wait(&childpid);
                     dup2(pipefds[0], STDIN_FILENO);
                     close(pipefds[1]);
                     
