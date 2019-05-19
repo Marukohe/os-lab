25f4721727a60c2e93c2f32a37e07be40d159d04
tracer-nju <tracer@nju.edu.cn>
2019-05-19 19:04:41 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 82c79d54683d4b4e823776f44111b97f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  19:04:41 up  9:43,  5 users,  load average: 1.54, 1.29, 1.29

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -182,6 +182,7 @@ int main(int argc, char *argv[]) {
                 uintptr_t bmpstart = (((uintptr_t)sdic->highCluster) << 16) + (uintptr_t)sdic->lowCluster - fatstruct->RootClusterNumber;
                 printf("%lx\n", (unsigned long)(bmpstart * SizeofCluster + RootCluster));
                 BITMAPFILEHEADER * bmp = (BITMAPFILEHEADER *)(bmpstart * SizeofCluster + RootCluster);
+                printf("bmpsize %lx\n", bmp->bfSize);
                 char bmpfile[bmp->bfSize];
                 snprintf(bmpfile, bmp->bfSize, "%s", (char *)(bmpstart * SizeofCluster + RootCluster));
                 FILE *fp = fopen(filename, "w+");
