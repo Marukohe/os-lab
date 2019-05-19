bdf8665f9db56162f6eb8b732d7ebbec46a71bdf
tracer-nju <tracer@nju.edu.cn>
2019-05-19 10:27:34 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 82c79d54683d4b4e823776f44111b97f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  10:27:34 up  1:06,  4 users,  load average: 0.74, 0.62, 0.56

--- frecov/frecov.c
+++ frecov/frecov.c
@@ -10,14 +10,36 @@
 #define handle_error(msg) \
     do { perror(msg); exit(EXIT_FAILURE); } while(0)
 
-struct FATstruct{
+typedef struct tagFATstruct{
     uint16_t BytesPerSector;
     uint8_t SectorsPerCluster;
     uint16_t ReservedSector;
     uint8_t NumberofFAT;
-}__attribute__((packed));
+    uint16_t RootEntries;
+    uint16_t SmallSector;
+    uint8_t MediaDes;
+    uint16_t SectorsPerFat16;
+    uint16_t SectorsPerTrack;
+    uint16_t NumberofHead;
+    uint32_t HiddenSector;
+    uint32_t LargeSector;
+    uint32_t SectorsPerFat;
+    uint16_t ExtendFlag;
+    uint16_t FilesystemVersion;
+    uint32_t RootClusterNumber;
+}__attribute__((packed)) FATstruct;
 
-struct FATstruct *fatstruct;
+FATstruct *fatstruct;
+
+typedef struct tagBITMAPFILEHEADER
+{
+    uint16_t bfType;//位图文件的类型，必须为BM(1-2字节）
+    uint32_t bfSize;//位图文件的大小，以字节为单位（3-6字节，低位在前）
+    uint16_t bfReserved1;//位图文件保留字，必须为0(7-8字节）
+    uint16_t bfReserved2;//位图文件保留字，必须为0(9-10字节）
+    uint32_t bfOffBits;//位图数据的起始位置，以相对于位图（11-14字节，低位在前）
+    //文件头的偏移量表示，以字节为单位
+}__attribute__((packed)) BITMAPFILEHEADER;
 
 unsigned long get_file_size(const char *path){
     unsigned long filesize = -1;
@@ -39,13 +61,21 @@ int main(int argc, char *argv[]) {
         handle_error("open");
     unsigned long fsize = get_file_size(argv[1]);
     startaddr = mmap(NULL, fsize, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-    fatstruct = (struct FATstruct *)(startaddr + 0xB);
+    fatstruct = (FATstruct *)(startaddr + 0xB);
+    
     printf("0x%x\n", fatstruct->BytesPerSector);
     printf("0x%x\n", fatstruct->SectorsPerCluster);
     printf("0x%x\n", fatstruct->ReservedSector);
-    /*printf("0x%x\n", *(startaddr + 0xE));*/
-    /*printf("0x%x\n", *(startaddr + 0xF));*/
     printf("0x%x\n", fatstruct->NumberofFAT);
+    printf("0x%x\n", fatstruct->RootEntries);
+    printf("0x%x\n", fatstruct->SmallSector);
+    printf("0x%x\n", fatstruct->MediaDes);
+    printf("0x%x\n", fatstruct->SectorsPerFat16);
+    printf("0x%x\n", fatstruct->SectorsPerTrack);
+    printf("0x%x\n", fatstruct->NumberofHead);
+    printf("0x%x\n", fatstruct->HiddenSector);
+    printf("0x%x\n", fatstruct->LargeSector);
+    printf("0x%x\n", fatstruct->SectorsPerTrack);
     munmap(startaddr, fsize);
     close(fd);
   return 0;
