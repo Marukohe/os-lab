5abb8ebda0cda2dbf5835069d3a7514dbcc21aa8
tracer-nju <tracer@nju.edu.cn>
2019-05-20 21:48:19 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: dbbfe5b954194cae81b133e340d6cf18   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  21:48:19 up  2:08,  8 users,  load average: 0.67, 0.75, 0.68

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -40,14 +40,14 @@ int kvdb_close(kvdb_t *db){
 int kvdb_put(kvdb_t *db, const char *key, const char *value){
     char *writechar = (char *)malloc(sizeof(char*));
     sprintf(writechar, "%s %s", key, value);
-    printf("%s\n", writechar);
-    printf("%ld\n", (unsigned long)strlen(writechar));
+    /*printf("%s\n", writechar);*/
+    /*printf("%ld\n", (unsigned long)strlen(writechar));*/
     int ret = write(db->fd, writechar, strlen(writechar));
     if(ret < 0){
         panic("write file failed");
         return -1;
     }
-
+    free(writechar);
     return 0;
 }
 
