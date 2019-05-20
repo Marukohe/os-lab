3c99fa1188bbb6a757bc24ae061af826bc18ace2
tracer-nju <tracer@nju.edu.cn>
2019-05-20 10:06:33 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: eb59c36371ca440cac74859844e3c4de   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  10:06:33 up  1:25,  5 users,  load average: 1.18, 1.00, 0.93

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -1,2 +1,17 @@
 #include "kvdb.h"
 
+int kvdb_open(kvdb_t * db, const char *filename){
+
+}
+
+int kvdb_close(kvdb_t *db){
+
+}
+
+int kvdb_put(kvdb_t *db, const char *key, const char *value){
+
+}
+
+char *kvdb_get(kvdb_t *db, const char *key){
+
+}
