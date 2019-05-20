f34dac3e34a6825a80b6f4e2bbf1e0b1c1aae45c
tracer-nju <tracer@nju.edu.cn>
2019-05-20 21:18:46 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: dbbfe5b954194cae81b133e340d6cf18   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  21:18:46 up  1:39,  8 users,  load average: 0.24, 0.43, 0.55

--- libkvdb/kvdb.c
+++ libkvdb/kvdb.c
@@ -8,20 +8,49 @@ int kvdb_open(kvdb_t * db, const char *filename){
     int ret = open(filename, O_CREAT | O_RDWR, 0666);
     printf("%d\n", ret);
     db->fd = ret;
-    if(ret < 0)
+    if(ret < 0){
         panic("open file failed");
+        return ret;
+    }
     return 0;
 }
+
+//====================================================
+//关闭数据库并释放相关资源。
+//====================================================
 
 int kvdb_close(kvdb_t *db){
-    if(db->fd <= 0)
+    if(db->fd < 0){
+        panic("close file failed, a fd < 0");
+        return -1;
+    }
+    int ret = close(db->fd);
+    if(ret < 0){
         panic("close file failed");
+        return -1;
+    }
     return 0;
 }
 
+//====================================================
+//建立key到value的映射
+//在kvdb_put执行之前db[key]已经有一个对应的字符串，它将被value覆盖。
+//====================================================
+
 int kvdb_put(kvdb_t *db, const char *key, const char *value){
+    int ret = write(db->fd, key, sizeof(key));
+    if(ret < 0){
+        panic("write file failed");
+        return -1;
+    }
+
     return 0;
 }
+
+//====================================================
+//获取key对应的value，相当于返回db[key]
+//动态分配,用malloc,使用之后要free.
+//===================================================
 
 char *kvdb_get(kvdb_t *db, const char *key){
     char *ret = "hello";
