e58bc9f83a9c3aa72ca287f09cdd8dad1b1d9f74
tracer-nju <tracer@nju.edu.cn>
2019-05-21 19:58:51 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 3a6c1c5b5f7144dcac732887d728a7a6   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  19:58:51 up  3:47,  8 users,  load average: 0.53, 0.56, 0.50

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -6,12 +6,13 @@
 #define TESTNUM 1000
 int main(){
     kvdb_t db;
-    char *key = "operating-systems";
+    /*char *key = "operating-systems";*/
+    char *key = (char *)malloc(sizeof(char)*1000);
     char *value;
 
     kvdb_open(&db, "a.db"); // BUG: should check for errors
     /*kvdb_put(&db, key, "three-easy-pieces");*/
-    char *buf = "operating-systems";
+    char *buf = (char *)malloc(sizeof(char)*1000);
     for(int i = 0; i < TESTNUM; i++){
         sprintf(buf, "operating-%d-systems", i);
         sprintf(key, "operating-%d-sys-hello", i);
@@ -32,5 +33,7 @@ int main(){
     kvdb_close(&db);
     /*printf("[%s]: [%s]\n", "hello", value);*/
     free(value);
+    free(key);
+    free(buf);
     return 0;
 }
