350468683d3f7f81f3fdae722abccc39296cf1d6
tracer-nju <tracer@nju.edu.cn>
2019-05-21 13:21:23 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 42480654f974422e87c2c9b930f7c47f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  13:21:23 up  3:34,  9 users,  load average: 0.71, 0.56, 0.46

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -12,6 +12,7 @@ int main(){
     kvdb_put(&db, key, "three-easy-pieces");
     kvdb_put(&db, key, "three-easy");
     kvdb_put(&db, "helloworld", "three-easy-hello");
+    kvdb_put(&db, key, "three-easy-pieces-imok");
     value = kvdb_get(&db, key);
     kvdb_close(&db);
     printf("[%s]: [%s]\n", key, value);
