3dc7293c5d2667fabcd31dbeb357a09e20884ad7
tracer-nju <tracer@nju.edu.cn>
2019-05-21 20:07:42 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 3a6c1c5b5f7144dcac732887d728a7a6   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  20:07:42 up  3:56,  8 users,  load average: 0.65, 0.66, 0.58

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -4,36 +4,38 @@
 #include <string.h>
 
 #define TESTNUM 100
-int main(){
-    kvdb_t db;
-    /*char *key = "operating-systems";*/
+void test1(int testnum){
     char *key = (char *)malloc(sizeof(char)*1000);
     char *value;
 
+    kvdb_t db;
     kvdb_open(&db, "a.db"); // BUG: should check for errors
     /*kvdb_put(&db, key, "three-easy-pieces");*/
     char *buf = (char *)malloc(sizeof(char)*1000);
-    for(int i = 0; i < TESTNUM; i++){
+    for(int i = 0; i < testnum; i++){
         sprintf(buf, "operating-%d-systems", i);
         sprintf(key, "operating-%d-sys-hello", i);
         kvdb_put(&db, buf, key);
     }
 
-    for(int i = 0; i < TESTNUM; i++){
+    for(int i = 0; i < testnum; i++){
         sprintf(buf, "operating-%d-systems", i);
         sprintf(key, "operating-%d-sys-hello-world", i);
         if((i  % 2) == 0)
             kvdb_put(&db, buf, key);
     }
-    for(int i = 0; i < TESTNUM; i++){
+    for(int i = 0; i < testnum; i++){
         sprintf(buf, "operating-%d-systems", i);
         value = kvdb_get(&db, buf);
         printf("[%s]: [%s]\n", buf, value);
+        free(value);
     }
-    kvdb_close(&db);
-    /*printf("[%s]: [%s]\n", "hello", value);*/
-    free(value);
     free(key);
     free(buf);
+    kvdb_close(&db);
+}
+
+int main(){
+    test1(100);
     return 0;
 }
