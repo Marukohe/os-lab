157e8408589fbfa78027841c89554732e168f36f
tracer-nju <tracer@nju.edu.cn>
2019-05-21 14:08:01 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 42480654f974422e87c2c9b930f7c47f   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  14:08:01 up  4:21, 10 users,  load average: 0.44, 0.55, 0.54

--- libkvdb/tests/main.c
+++ libkvdb/tests/main.c
@@ -10,9 +10,9 @@ int main(){
 
     kvdb_open(&db, "a.db"); // BUG: should check for errors
     kvdb_put(&db, key, "three-easy-pieces");
-    kvdb_put(&db, key, "three-easy");
-    kvdb_put(&db, "helloworld", "three-easy-hello");
-    kvdb_put(&db, key, "three-easy-pieces-imok");
+    /*kvdb_put(&db, key, "three-easy");*/
+    /*kvdb_put(&db, "helloworld", "three-easy-hello");*/
+    /*kvdb_put(&db, key, "three-easy-pieces-imok");*/
     value = kvdb_get(&db, key);
     kvdb_close(&db);
     printf("[%s]: [%s]\n", key, value);
