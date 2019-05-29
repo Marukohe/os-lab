3749b89ed949b368a3e332b1c6b820e0aea3237c
tracer-nju <tracer@nju.edu.cn>
2019-05-29 11:04:24 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: a4e04282ee894176bdff19b3e6ca8319   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  11:04:24 up  2:28,  5 users,  load average: 0.29, 0.40, 0.52

---
+++ kernel/include/vfs.h
@@ -0,0 +1,61 @@
+#include <common.h>
+
+typedef struct {
+  void (*init)();
+  int (*access)(const char *path, int mode);
+  int (*mount)(const char *path, filesystem_t *fs);
+  int (*unmount)(const char *path);
+  int (*mkdir)(const char *path);
+  int (*rmdir)(const char *path);
+  int (*link)(const char *oldpath, const char *newpath);
+  int (*unlink)(const char *path);
+  int (*open)(const char *path, int flags);
+  ssize_t (*read)(int fd, void *buf, size_t nbyte);
+  ssize_t (*write)(int fd, void *buf, size_t nbyte);
+  off_t (*lseek)(int fd, off_t offset, int whence);
+  int (*close)(int fd);
+} MOD_NAME(vfs);
+
+struct filesystem {
+  //...
+  fsops_t *ops;
+  dev_t *dev;
+};
+
+typedef struct fsops {
+  void (*init)(struct filesystem *fs, const char *name, dev_t *dev);
+  inode_t *(*lookup)(struct filesystem *fs, const char *path, int flags);
+  int (*close)(inode_t *inode);
+} fsops_t;
+
+typedef struct inodeops {
+  int (*open)(file_t *file, int flags);
+  int (*close)(file_t *file);
+  ssize_t (*read)(file_t *file, char *buf, size_t size);
+  ssize_t (*write)(file_t *file, const char *buf, size_t size);
+  off_t (*lseek)(file_t *file, off_t offset, int whence);
+  int (*mkdir)(const char *name);
+  int (*rmdir)(const char *name);
+  int (*link)(const char *name, inode_t *inode);
+  int (*unlink)(const char *name);
+  // 你可以自己设计readdir的功能
+} inodeops_t;
+
+struct inode {
+  //...
+  int refcnt;
+  void *ptr;       // private data
+  filesystem_t *fs;
+  inodeops_t *ops; // 在inode被创建时，由文件系统的实现赋值
+                   // inode ops也是文件系统的一部分
+  //...
+};
+
+typedef struct file {
+  int refcnt; // 引用计数
+  inode_t *inode;
+  uint64_t offset;
+  //...
+} file_t;
+
+
