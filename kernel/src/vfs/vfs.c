267c066dd161433610203c9f03f3c9f5d484dc04
tracer-nju <tracer@nju.edu.cn>
2019-05-29 18:19:28 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: e4109c2a967642b88455dba91d5f0619   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  18:19:28 up 45 min,  6 users,  load average: 0.65, 0.83, 0.92

--- kernel/src/vfs/vfs.c
+++ kernel/src/vfs/vfs.c
@@ -1,3 +1,22 @@
 #include <common.h>
 #include <vfs.h>
 
+
+
+
+
+MODULE_DEF(vfs){
+    .init = init,
+    .access = access,
+    .mount = mount,
+    .unmount = unmount,
+    .mkdir = mkdir,
+    .rmdir = rmdir,
+    .link = link,
+    .unlink = unlink,
+    .open = open,
+    .read = read,
+    .write = write,
+    .lseek = lseek,
+    .close = close,
+};
