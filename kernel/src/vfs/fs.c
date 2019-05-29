81ba2821f5cfd4da82875978ebc784d706f58a42
tracer-nju <tracer@nju.edu.cn>
2019-05-29 20:12:51 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: e4109c2a967642b88455dba91d5f0619   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  20:12:51 up  2:38,  6 users,  load average: 0.69, 1.03, 1.06

--- kernel/src/vfs/fs.c
+++ kernel/src/vfs/fs.c
@@ -2,7 +2,7 @@
 #include <vfs.h>
 extern void TODO();
 
-void init(struct filesystem *fs, const char *name, device_t *dev){
+void fsinit(struct filesystem *fs, const char *name, device_t *dev){
     TODO();
     return;
 }
@@ -12,7 +12,7 @@ inode_t *lookup(struct filesystem *fs, const char *path, int flags){
     return NULL;
 }
 
-int close(inode_t *inode){
+int fsclose(inode_t *inode){
     TODO();
     return 0;
 }
