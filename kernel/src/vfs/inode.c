3001c7b9b1c07c7caf2bbe5535d3c55053ebb0b2
tracer-nju <tracer@nju.edu.cn>
2019-05-29 20:10:00 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: e4109c2a967642b88455dba91d5f0619   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  20:10:00 up  2:35,  6 users,  load average: 1.23, 1.12, 1.08

--- kernel/src/vfs/inode.c
+++ kernel/src/vfs/inode.c
@@ -3,47 +3,47 @@
 #include <klib.h>
 extern void TODO();
 
-int open(file_t *file, int flags){
+int inodeopen(file_t *file, int flags){
     TODO();
     return 0;
 }
 
-int close(file_t *file){
+int inodeclose(file_t *file){
     TODO();
     return 0;
 }
 
-ssize_t read(file_t *file, char *buf, size_t size){
+ssize_t inoderead(file_t *file, char *buf, size_t size){
     TODO();
     return 0;
 }
 
-ssize_t write(file_t *file, const char *buf, size_t size){
+ssize_t inodewrite(file_t *file, const char *buf, size_t size){
     TODO();
     return 0;
 }
 
-off_t lseek(file_t *file, off_t offset, int whence){
+off_t inodelseek(file_t *file, off_t offset, int whence){
     TODO();
     return 0;
 }
 
-int mkdir(const char *name){
+int inodemkdir(const char *name){
     TODO();
     return 0;
 }
 
-int rmdir(const char *name){
+int inodermdir(const char *name){
     TODO();
     return 0;
 }
 
-int link(const char *name, inode_t *inode){
+int inodelink(const char *name, inode_t *inode){
     TODO();
     return 0;
 }
 
-int unlink(const char *name){
+int inodeunlink(const char *name){
     TODO();
     return 0;
 }
