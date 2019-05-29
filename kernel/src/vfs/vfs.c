4bbe525526c9b29194d818290017e6778485e8be
tracer-nju <tracer@nju.edu.cn>
2019-05-29 18:22:54 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: e4109c2a967642b88455dba91d5f0619   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  18:22:54 up 48 min,  6 users,  load average: 0.88, 0.82, 0.90

--- kernel/src/vfs/vfs.c
+++ kernel/src/vfs/vfs.c
@@ -1,8 +1,73 @@
 #include <common.h>
 #include <vfs.h>
+void TODO(){
+    assert(0);
+}
 
+void (*init)(){
+    TODO();
+    return;
+}
 
+int (*access)(const char *path, int mode){
+    TODO();
+    return 0;
+}
 
+int (*mount)(const char *path, filesystem_t *fs){
+    TODO();
+    return 0;
+}
+
+int (*unmount)(const char *path){
+    TODO();
+    return 0;
+}
+
+int (*mkdir)(const char *path){
+    TODO();
+    return 0;
+}
+
+int (*rmdir)(const char *path){
+    TODO();
+    return 0;
+}
+
+int (*link)(const char *oldpath, const char *newpath){
+    TODO();
+    return 0;
+}
+
+int (*unlink)(const char *path){
+    TODO();
+    return 0;
+}
+
+int (*open)(const char *path, int flags){
+    TODO();
+    return 0;
+}
+
+ssize_t (*read)(int fd, void *buf, size_t nbyte){
+    TODO();
+    return 0;
+}
+
+ssize_t (*write)(int fd, void *buf, size_t nbyte){
+    TODO();
+    return 0;
+}
+
+off_t (*lseek)(int fd, off_t offset, int whence){
+    TODO();
+    return 0;
+}
+
+int (*close)(int fd){
+    TODO();
+    return 0;
+}
 
 
 MODULE_DEF(vfs){
