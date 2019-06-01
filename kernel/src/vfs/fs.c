2ecf12643b2838f64038f7b6b616257c9a88243d
tracer-nju <tracer@nju.edu.cn>
2019-06-01 10:37:58 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: dc84b0d60c3e48808e44b1d4c31e3f76   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  10:37:58 up  1:18,  8 users,  load average: 0.44, 0.41, 0.38

--- kernel/src/vfs/fs.c
+++ kernel/src/vfs/fs.c
@@ -1,8 +1,12 @@
 #include <common.h>
 #include <vfs.h>
 #include <devices.h>
+#include <klib.h>
 extern void TODO();
 extern inodeops_t inode_ops;
+#define OFFSET (sizeof(inode_t))
+#define BLOCKSIZE 4096
+#define DIRSIZE 512
 
 void fsinit(struct filesystem *fs, const char *name, device_t *dev){
     /*TODO();*/
@@ -18,8 +22,28 @@ void fsinit(struct filesystem *fs, const char *name, device_t *dev){
     return;
 }
 
+static void getpath(char *get, const char *path, int offset){
+    const char *cp;
+    cp = path + offset;
+    int len = strlen(path);
+    while(*cp != '/' && offset < len){
+        *get++ = *cp++;
+        offset++;
+    }
+    *get = '\0';
+}
+
 inode_t *lookup(struct filesystem *fs, const char *path, int flags){
-    TODO();
+    /*TODO();*/
+    int offset = 0;
+    char *get = (char *)pmm->alloc(sizeof(DIRSIZE));
+    while(offset < strlen(path)){
+        getpath(get, path, offset);
+        printf("%s\n", get);
+        offset += strlen(get) + 1;
+    }
+    pmm->free(get);
+
     return NULL;
 }
 
