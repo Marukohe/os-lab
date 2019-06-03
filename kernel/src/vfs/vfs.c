7364502422645e0814f20d1e2afa10a4298d2b79
tracer-nju <tracer@nju.edu.cn>
2019-06-03 18:30:08 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: fdf05ad9e60a4e8cbdf4d45cf8f728db   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  18:30:08 up 14 min,  7 users,  load average: 0.23, 0.50, 0.40

--- kernel/src/vfs/vfs.c
+++ kernel/src/vfs/vfs.c
@@ -75,6 +75,9 @@ void vfstest(){
     vfs->mkdir("/hello/a");
     ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", 7|O_DIR);
     check(ret);
+    vfs->rmfir("hello/a");
+    ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", 7|O_DIR);
+    check(ret);
     /*ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", O_DIR);*/
     /*check(ret);*/
     /*ret = filesys[2]->ops->lookup(filesys[2], "/hello/a/c.txt", 7|O_CREAT);*/
