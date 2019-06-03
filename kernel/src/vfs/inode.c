4883ea0589196e88946c6bac4a9481be81e20edd
tracer-nju <tracer@nju.edu.cn>
2019-06-03 18:40:39 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: fdf05ad9e60a4e8cbdf4d45cf8f728db   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  18:40:39 up 24 min,  7 users,  load average: 0.59, 0.68, 0.54

--- kernel/src/vfs/inode.c
+++ kernel/src/vfs/inode.c
@@ -67,6 +67,7 @@ int inodemkdir(const char *name){
 
 char *splitpath(char *path, int offset){
     int t = 0;
+    Logp("%s %d", path, offset);
     char *ret = pmm->alloc(NAMELEN);
     for(int i = offset - 1; i >= 0; i--){
         Logp("%c", path[i]);
