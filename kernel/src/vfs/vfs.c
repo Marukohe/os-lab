59b31e0c97a7f030fdfea1b434014a84d224eedb
tracer-nju <tracer@nju.edu.cn>
2019-06-03 18:54:20 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: fdf05ad9e60a4e8cbdf4d45cf8f728db   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  18:54:20 up 38 min,  7 users,  load average: 0.25, 0.47, 0.55

--- kernel/src/vfs/vfs.c
+++ kernel/src/vfs/vfs.c
@@ -71,14 +71,14 @@ void vfstest(){
     const char *path = pmm->alloc(100);
     path = "/hello";
     vfs->mkdir(path);
-    inode_t *ret = filesys[2]->ops->lookup(filesys[2], path, 7|O_DIR);
-    check(ret);
-    vfs->mkdir("/hello/a");
-    ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", 7|O_DIR);
-    check(ret);
-    vfs->rmdir("/hello/a");
-    ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", 7|O_DIR);
-    check(ret);
+    /*inode_t *ret = filesys[2]->ops->lookup(filesys[2], path, 7|O_DIR);*/
+    /*check(ret);*/
+    /*vfs->mkdir("/hello/a");*/
+    /*ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", 7|O_DIR);*/
+    /*check(ret);*/
+    /*vfs->rmdir("/hello/a");*/
+    /*ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", 7|O_DIR);*/
+    /*check(ret);*/
     /*ret = filesys[2]->ops->lookup(filesys[2], "/hello/a", O_DIR);*/
     /*check(ret);*/
     /*ret = filesys[2]->ops->lookup(filesys[2], "/hello/a/c.txt", 7|O_CREAT);*/
