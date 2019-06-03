f02ec28acc8149314a20d8e09175dfac96aa40aa
tracer-nju <tracer@nju.edu.cn>
2019-06-03 20:07:30 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: fdf05ad9e60a4e8cbdf4d45cf8f728db   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  20:07:30 up  1:51,  5 users,  load average: 0.10, 0.19, 0.13

--- kernel/src/vfs/inode.c
+++ kernel/src/vfs/inode.c
@@ -93,20 +93,39 @@ int inodermdir(const char *name){
     int offset = strlen(sonpath);
     /*Logw("%s", sonpath);*/
     char *fapath = splitpath(sonpath, offset);
-    Logw("%s", fapath);
+    /*Logw("%s", fapath);*/
     inode_t *fa;
     if(fapath == NULL){
-        Logw("hello?");
+        /*Logw("hello?");*/
         fa = filesys[id]->sinode;
     }else{
         fa = filesys[id]->ops->lookup(filesys[id], fapath, 7|O_DIR);
     }
     inode_t *son = filesys[id]->ops->lookup(filesys[id], sonpath, 7|O_DIR);
-    Logw("fa pos: %x, fa->offset: %x", fa->pos, fa->offset[0]);
+    /*Logw("fa pos: %x, fa->offset: %x", fa->pos, fa->offset[0]);*/
     //更新fa的目录项
     void *buf = pmm->alloc(BLOCKSIZE);
-    filesys[2]->dev->ops->read(filesys[2]->dev, fa->offset[0], buf, BLOCKSIZE);
+    filesys[2]->dev->ops->read(filesys[2]->dev, son->offset[0], buf, BLOCKSIZE);
     dir_t *dir = (dir_t *)buf;
+    int cntsonnode = 0;
+    for(int i = 0; i < dir->cnt; i++){
+        if(dir->used[i])
+            cntnode++;
+    }
+    if(cntnode > 0){
+        printf("rmdir failed. This is not an empty dic\n");
+        pmm->free(buf);
+        pmm->free(sonpath);
+        pmm->free(fapath);
+        pmm->free(son);
+        pmm->free(fa);
+        return -1;
+    }
+
+
+    memset(buf, 0, BLOCKSIZE);
+    filesys[2]->dev->ops->read(filesys[2]->dev, fa->offset[0], buf, BLOCKSIZE);
+    dir = (dir_t *)buf;
     for(int i = 0; i < dir->cnt; i++){
         if(dir->used[i] == 1 && dir->offset[i] == son->pos){
             printf("rmdir successfully!\n");
@@ -121,6 +140,11 @@ int inodermdir(const char *name){
             break;
         }
     }
+    pmm->free(buf);
+    pmm->free(sonpath);
+    pmm->free(fapath);
+    pmm->free(son);
+    pmm->free(fa);
 
     return 0;
 }
