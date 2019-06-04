69ab2fa21c512ee7ad8e8fb4d1d51731c305657b
tracer-nju <tracer@nju.edu.cn>
2019-06-04 17:32:00 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: a6b4456ef84f40f3a08c289d43215df7   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  17:32:00 up 8 min,  7 users,  load average: 0.26, 0.30, 0.18

--- kernel/src/shell.c
+++ kernel/src/shell.c
@@ -4,6 +4,35 @@
 extern spinlock_t shelllock;
 extern spinlock_t yk;
 
+char *strsplit(char *s1){
+    char *cp = pmm->alloc(128);
+    int off = 0;
+    for(int i = 0; i < strlen(s1); i++){
+        if(s1[i] == ' '){
+            off = i;
+            strncpy(cp, s1, off - 1);
+            return cp;
+        }
+    }
+    pmm->free(cp);
+    return NULL;
+}
+
+static int shell_help(char *args){
+    return 0;
+}
+
+
+static struct{
+    char *name;
+    char *descrition;
+    int (*handler) (char *);
+} shell_table [] = {
+    {"help", "Display imformations about supported commands", shell_help},
+};
+
+#define NR_SHELL (sizeof(shell_table) / sizeof(shell_table[0]))
+
 void shell(void *name){
     /*kmt->spin_lock(&shelllock);*/
     int stdin = vfs->open(name, RABLE);
@@ -15,6 +44,9 @@ void shell(void *name){
         int nread = vfs->read(stdin, line, sizeof(line));
         line[nread - 1] = '\0';
         sprintf(text, "Echo: %s.\n", line);
+        char *cmd = strsplit(line);
+        char args = name + strlen(cmd) + 1;
+        Logy("cmd: %s args: %s", cmd, args);
         vfs->write(stdout, text, strlen(text));
     }
     /*kmt->spin_unlock(&shelllock);*/
