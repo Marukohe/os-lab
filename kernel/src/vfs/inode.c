cd41f655f5ae0a44d0920931b72da853d3f08e6d
tracer-nju <tracer@nju.edu.cn>
2019-06-05 11:41:51 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 0a9c9c6e16814370b085a7909fb2ac78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  11:41:51 up  2:15,  7 users,  load average: 0.30, 0.36, 0.39

--- kernel/src/vfs/inode.c
+++ kernel/src/vfs/inode.c
@@ -134,7 +134,7 @@ int inodemkdir(const char *name){
     /*printf("path decode: %s\n", path);*/
     int offset = strlen(path);
     char *fapath = splitpath(path, offset);
-    ret = filesys[id]->ops->lookup(filesys[id], fapath, 7|O_DIR);
+    int ret = filesys[id]->ops->lookup(filesys[id], fapath, 7|O_DIR);
     if(ret == NULL){
         pmm->free(path);
         pmm->free(fapath);
