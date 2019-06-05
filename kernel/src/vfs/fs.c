97228418d1c008b999cd2e2668327d31a57050d0
tracer-nju <tracer@nju.edu.cn>
2019-06-05 10:40:05 +0800

    Static hostname: snow          Icon name: computer-laptop            Chassis: laptop         Machine ID: f8a9849ce3624890b75d987de2d65d49            Boot ID: 0a9c9c6e16814370b085a7909fb2ac78   Operating System: Ubuntu 18.04.2 LTS             Kernel: Linux 5.0.0-050000rc8-generic       Architecture: x86-64  10:40:05 up  1:13,  7 users,  load average: 0.57, 0.66, 0.55

--- kernel/src/vfs/fs.c
+++ kernel/src/vfs/fs.c
@@ -83,6 +83,7 @@ inode_t *lookup(struct filesystem *fs, const char *path, int flags){
         uint8_t inodefind = 0;
         for(int i = 0; i < dir->cnt; i++){
             if(dir->used[i] == 0) continue;
+            Logp("node permission: %d get: %s dir->name[i]: %s\n", ret->flags, get, dir->name[i]);
             if(strcmp(dir->name[i], get) == 0){
                 //获取目录块中记录的inode
                 filesys[2]->dev->ops->read(filesys[2]->dev, dir->offset[i], tmpnode, INODESIZE);
